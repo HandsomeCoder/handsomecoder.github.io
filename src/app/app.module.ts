5fbc36cb3a5ded9ae42c45ac6116c683a537299c
handsomecoder <harshshah1295@gmail.com>
2024-06-30 14:56:38 -0400

    Updated to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BasicInfoComponent } from './basic-info/basic-info.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { SanitizeHTMLPipe } from './pipes/sanitize-html.pipe';
 import { I18nPipe } from './pipes/i18n.pipe';
 import { ExperienceInfoComponent } from './experience-info/experience-info.component';
@@ -13,24 +13,17 @@ import { BadgeInfoComponent } from './badge-info/badge-info.component';
 import { CertificationInfoComponent } from './certification-info/certification-info.component';
 import { DatetimePipe } from './pipes/datetime.pipe';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    BasicInfoComponent,
-    ContactInfoComponent,
-    SanitizeHTMLPipe,
-    I18nPipe,
-    ExperienceInfoComponent,
-    SkillsInfoComponent,
-    BadgeInfoComponent,
-    CertificationInfoComponent,
-    DatetimePipe
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        BasicInfoComponent,
+        ContactInfoComponent,
+        SanitizeHTMLPipe,
+        I18nPipe,
+        ExperienceInfoComponent,
+        SkillsInfoComponent,
+        BadgeInfoComponent,
+        CertificationInfoComponent,
+        DatetimePipe
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
